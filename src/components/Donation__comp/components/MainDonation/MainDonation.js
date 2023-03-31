4fd95d0a52463590bcdfb12d0b7b47a36d8c69f7
Andriy Doroshevych <doroshevychandrew@gmail.com>
2023-03-31 23:01:48 +0200

    add

--- src/components/Donation__comp/components/MainDonation/MainDonation.js
+++ src/components/Donation__comp/components/MainDonation/MainDonation.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 export class MainDonation extends Component {
 
 componentDidMount(){
-    const a = document.querySelector('.component__main__donation');
+    const mainDonation = document.querySelector('.component__main__donation');
     const header = document.querySelector('.component__header');
     header.classList.remove('header__active')
-    if(a){
+    if(mainDonation){
         header.classList.add('header__active');
     }
 }
@@ -14,18 +14,18 @@ componentDidMount(){
 
   render() {
     return (
-            <div className="main__content">
-                <div className="main__title">
-                    <div className="part">
-                        <hr />
-                        <div className="part__text">Donate</div>
-                    </div>
-                    <div className="main__title-text">Making a donation for Nature.</div>
-                    <div className="main__subtitle-text">When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.</div>
-                    <div className="main__donation__btn btn">Donate now</div>
+        <div className="main__content">
+            <div className="main__title">
+                <div className="part">
+                    <hr />
+                    <div className="part__text">Donate</div>
                 </div>
-                <div className="main__donation__img"></div>
+                <div className="main__title-text">Making a donation for Nature.</div>
+                <div className="main__subtitle-text">When you donate, you’re supporting effective solutions to big environmental challenges—an investment in the future of our planet.</div>
+                <div className="main__donation__btn btn">Donate now</div>
             </div>
+            <div className="main__donation__img"></div>
+        </div>
     )
   }
 }
